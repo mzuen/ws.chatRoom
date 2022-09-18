60a0eaa6ba7398b71ea65760131a05724c499fb8
mzuen <meitzuen@gmail.com>
2022-09-18 17:23:13 +0800

    feat: add system notification

--- ws/main.py
+++ ws/main.py
@@ -2,6 +2,7 @@ from typing import List
 
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 from fastapi.responses import HTMLResponse
+
 html = open("templates/index.html").read()
 
 app = FastAPI()
@@ -11,9 +12,10 @@ class ConnectionManager:
     def __init__(self):
         self.active_connections: List[WebSocket] = []
 
-    async def connect(self, websocket: WebSocket):
+    async def connect(self, client_id: str, websocket: WebSocket):
         await websocket.accept()
         self.active_connections.append(websocket)
+        self.broadcast(f"系統通知: {client_id} 已進入聊天室")
 
     def disconnect(self, websocket: WebSocket):
         self.active_connections.remove(websocket)
@@ -28,6 +30,7 @@ class ConnectionManager:
 
 manager = ConnectionManager()
 
+USERS = []
 
 @app.get("/")
 async def get():
@@ -36,11 +39,12 @@ async def get():
 
 @app.websocket("/ws/{client_id}")
 async def websocket_endpoint(websocket: WebSocket, client_id: str):
-    await manager.connect(websocket)
+    await manager.connect(client_id, websocket)
     try:
         while True:
             data = await websocket.receive_text()
             await manager.broadcast(f"#{client_id}: {data}")
+
     except WebSocketDisconnect:
         manager.disconnect(websocket)
         await manager.broadcast(f"#{client_id} 已離開聊天室")
